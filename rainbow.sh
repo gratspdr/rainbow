4bc57455fc81dccafa9a3a63ffd6c20ead425c6b
ghcees source <ghceescheryl@outlook.com>
2024-04-08 21:27:20 +0000

    Update rainbow.sh

--- rainbow.sh
+++ rainbow.sh
@@ -1,42 +1,3 @@
-N = '\033[0m'
-D = '\033[90m'
-W = '\033[1;37m'
-B = '\033[1;34m'
-R = '\033[1;31m'
-G = '\033[1;32m'
-Y = '\033[1;33m'
-C = '\033[1;36m'
-
-ask = G + '[' + W + '?' + G + '] '
-sukses = G + '[' + W + '√' + G + '] '
-eror = R + '[' + W + '!' + R + ']'
-
-banner = """
-{}         _nnnn_{}        _________________
-{}        dGGGGMMb{}      |                 |
-{}       @p~qp~~qMb{}   ._| {}Bash Oubfuscate {}|
-{}       M{}({}@{})({}@{}) {}M|{}  /  |_________________|
-{}       @,{}----.{}JM|{}_/
-{}      JS^{}\__/{}  qKL
-     dZP        qKRb
-    dZP          qKKb
-   fZP            SMMb
-   HZM            MMMM    {}Coded by {}: {}Syhrül
-{}   FqM            MMMM    {}FB       {}: {}fb.com/sarul.arif.5
-{} __|'\ .        |\{}dS qML
-{} |    `.       | `' \{}Zq
-{}_)      \.{}___.{},|     .'
-\____   ){}MMMMMP{}|   .'
-     `-'       `--'
-""".format(D,W,D,W,D,W,Y,W,D,W,D,W,D,W,D,W,D,Y,D,W,D,Y,D,G,W,G,D,G,W,G,Y,D,Y,D,Y,D,Y,D,Y)
-
-banner2 = """
-   {}[{}1{}]{} Encript      {}[{}2{}]{} Decrypt
-""".format(G,W,G,W,G,W,G,W)
-
-print banner
-print banner2
-
 red='\033[0;91m'
 green='\033[1;92m'
 yellow='\033[1;93m'
@@ -55,6 +16,8 @@ sleep 1
 TIK=$(shuf -n 1 token.txt)
 USER=$1
 sleep 1
+sudo apt-get install sysvbanner > /dev/null 2>&1
+sudo apt install figlet > /dev/null 2>&1
 rm -rf ngrok  ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 wget -qO ngrok-v3-stable-linux-amd64.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xvf ngrok-v3-stable-linux-amd64.tgz  > /dev/null 2>&1
@@ -77,3 +40,5 @@ echo -e "$green"
 curl ipinfo.io/org
 curl ipinfo.io/country
 echo -e "$clear"
+figlet Finish the Job
+TZ=UTC-7 date +%R-%d/%m/%y
